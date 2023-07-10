ea9bf93296abf4e4825c0b3ce4c2a61aa55cc76b
Mohammad harb <136888573+MohammadHb82@users.noreply.github.com>
2023-07-10 10:56:26 -0700

    homework2
    homework 2 slicing and indexing

---
+++ homework2.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 10:27:57 2023
+
+@author: ASUS
+"""
+#data type
+#int1=20
+#float1=20.0
+#bool1=True
+#string1="mohammad"
+#complex1=10+10j
+
+#print("<___________>")
+
+#print(type(int1))
+
+#print(type(float1))
+
+#print(type(complex1))
+
+#print(type(string1))
+
+#print("<____________>")
+#indexing
+#indexing_value="Jaxon"
+
+#print(indexing_value[0])
+
+#print(indexing_value[4])
+
+#print(indexing_value[-1])
+
+#print(indexing_value[:])
+
+#print(indexing_value[1:])
+
+#print(indexing_value[-1:])
+
+#print(indexing_value[:-1])
+
+#print("<____________>")
+#slicing and swap using clicing
+
+#str1="hello"
+#str2="y" +str1[1:len(str1)]
+#print(str2)
+#str3="mohammad"
+#str4="Ha" + str3[2:len(str3)]
+#print(str4)
+#str5="Ha"+str3
+#print(str5)
+#str7="hello"
+#str8=str7[0:2] +"mm"+str7[4:]
+#print(str8)
+#str1="hello there"
+#str2="y" + str1[4:len(str1)]
+#print(str2)
+#str3="hello there"
+#str4="Ha" + str3[1:len(str3)]
+#print(str4)
+#str5="hello there"
+#str6=str5[0:3] +"mm" +str5[:]
+#print(str6)
+#print("<_____________>")
